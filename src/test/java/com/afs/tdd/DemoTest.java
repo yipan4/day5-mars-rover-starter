11f42734a9c970f5fb51d458abab0422a959b9fd
Andy Yip <andy.yip@oocl.com>
2025-09-05 16:17:07 +0800

    Test: Added test cases for turnRight

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -10,6 +10,7 @@ class DemoTest {
         assertEquals(expectedLocation.getDirection(), currentLocation.getDirection());
     }
 
+    // Turn Left
     @Test
     void should_face_N_when_turnLeft_given_E() {
         // Given
@@ -57,6 +58,58 @@ class DemoTest {
         String command = "L";
         // Expect
         Location expectedLocation = new Location(0,0,Direction.E);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_face_W_when_turnLeft_given_S() {
+        // Given
+        Location location = new Location(Direction.S);
+        // When
+        String command = "L";
+        // Expect
+        Location expectedLocation = new Location(0,0,Direction.W);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_face_S_when_turnRight_given_E() {
+        // Given
+        Location location = new Location(Direction.E);
+        // When
+        String command = "R";
+        // Expect
+        Location expectedLocation = new Location(0,0,Direction.S);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_face_E_when_turnRight_given_N() {
+        // Given
+        Location location = new Location(Direction.E);
+        // When
+        String command = "R";
+        // Expect
+        Location expectedLocation = new Location(0,0,Direction.W);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_face_N_when_turnRight_given_W() {
+        // Given
+        Location location = new Location(Direction.W);
+        // When
+        String command = "R";
+        // Expect
+        Location expectedLocation = new Location(0,0,Direction.N);
         MarsRover marsRover = new MarsRover(location);
         Location currentLocation = marsRover.ExecuteBatchCommand(command);
         assertLocationEquals(expectedLocation, currentLocation);
