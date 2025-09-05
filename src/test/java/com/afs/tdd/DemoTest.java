cb424a6865712092db3ee9afd4c13dbff80ac58c
Andy Yip <andy.yip@oocl.com>
2025-09-05 16:35:18 +0800

    Added test case for move

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     void assertLocationEquals(Location expectedLocation, Location currentLocation) {
-        assertEquals(expectedLocation.getLocationY(), currentLocation.getLocationX());
+        assertEquals(expectedLocation.getLocationX(), currentLocation.getLocationX());
         assertEquals(expectedLocation.getLocationY(), currentLocation.getLocationY());
         assertEquals(expectedLocation.getDirection(), currentLocation.getDirection());
     }
@@ -111,6 +111,59 @@ class DemoTest {
         String command = "R";
         // Expect
         Location expectedLocation = new Location(0,0,Direction.N);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    // Move
+    @Test
+    void should_at_0_1_N_when_move_given_0_0_N() {
+        // Given
+        Location location = new Location(Direction.N);
+        // When
+        String command = "M";
+        // Expect
+        Location expectedLocation = new Location(0,1,Direction.N);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_at_1_0_E_when_move_given_0_0_E() {
+        // Given
+        Location location = new Location(Direction.E);
+        // When
+        String command = "M";
+        // Expect
+        Location expectedLocation = new Location(1,0,Direction.E);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_at_0_neg1_S_when_move_given_0_0_S() {
+        // Given
+        Location location = new Location(Direction.S);
+        // When
+        String command = "M";
+        // Expect
+        Location expectedLocation = new Location(0,-1,Direction.S);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_at_neg1_0_W_when_move_given_0_0_W() {
+        // Given
+        Location location = new Location(Direction.W);
+        // When
+        String command = "M";
+        // Expect
+        Location expectedLocation = new Location(-1,0,Direction.W);
         MarsRover marsRover = new MarsRover(location);
         Location currentLocation = marsRover.ExecuteBatchCommand(command);
         assertLocationEquals(expectedLocation, currentLocation);
