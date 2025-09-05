9d4a9a8ad0c02292638db288f8ad286c81290eed
Andy Yip <andy.yip@oocl.com>
2025-09-05 16:46:54 +0800

    Test: Added test cases for move backward

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -168,4 +168,57 @@ class DemoTest {
         Location currentLocation = marsRover.ExecuteBatchCommand(command);
         assertLocationEquals(expectedLocation, currentLocation);
     }
+
+    // Move backward
+    @Test
+    void should_at_0_neg1_N_when_move_backward_given_0_0_N() {
+        // Given
+        Location location = new Location(Direction.N);
+        // When
+        String command = "M";
+        // Expect
+        Location expectedLocation = new Location(0,-1,Direction.N);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_at_neg1_0_E_when_move_backward_given_0_0_E() {
+        // Given
+        Location location = new Location(Direction.E);
+        // When
+        String command = "M";
+        // Expect
+        Location expectedLocation = new Location(-1,0,Direction.E);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_at_0_1_S_when_move_backward_given_0_0_S() {
+        // Given
+        Location location = new Location(Direction.S);
+        // When
+        String command = "M";
+        // Expect
+        Location expectedLocation = new Location(0,1,Direction.S);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
+
+    @Test
+    void should_at_1_0_W_when_move_backward_given_0_0_W() {
+        // Given
+        Location location = new Location(Direction.W);
+        // When
+        String command = "M";
+        // Expect
+        Location expectedLocation = new Location(1,0,Direction.W);
+        MarsRover marsRover = new MarsRover(location);
+        Location currentLocation = marsRover.ExecuteBatchCommand(command);
+        assertLocationEquals(expectedLocation, currentLocation);
+    }
 }
